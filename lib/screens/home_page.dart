26dd3d05596060845fa3d91f6d9d27ece7b25abc
hanaa_2000 <Hanaashrief75@gmail.com>
2023-03-26 19:40:59 +0200

    Create_Home

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
+
 class Home_Page extends StatelessWidget {
   const Home_Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ;
   }
 }
