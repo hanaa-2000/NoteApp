3a912cfda7a74f04437d196fa22181e2d20ddcb0
hanaa_2000 <Hanaashrief75@gmail.com>
2023-03-27 21:48:41 +0200

    Add show bottom sheet

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgits/add_note_bottom_sheet.dart';
 import 'package:note_app/widgits/note_view_body.dart';
 
 class Home_Page extends StatelessWidget {
@@ -7,7 +8,22 @@ class Home_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NoteViewBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              context: context,
+              builder: (context){
+            return const AddNoteBottomSheet();
+          });
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: const NoteViewBody(),
     );
   }
 }
