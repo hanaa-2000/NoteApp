683b5ca0fbc8eb0030ce84be4c393be724c78eb7
hanaa_2000 <Hanaashrief75@gmail.com>
2023-04-01 16:22:44 +0200

    Create register note and cubit

--- lib/cubits/cubit.dart
+++ lib/cubits/cubit.dart
@@ -2,14 +2,25 @@
 
 
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constant/constant.dart';
 import 'package:note_app/cubits/state.dart';
 import 'package:note_app/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState>{
   AddNoteCubit():super(AddNoteInitial());
 
-  addNote(NoteModel note){
+  addNote(NoteModel note) async{
+    emit(AddNoteLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSuccess());
+      await noteBox.add(note);
 
+    }
+    catch(e){
+      emit(AddNoteFailure(e.toString()));
+    }
   }
 
 
